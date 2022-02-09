0195fc79fa339797982a4bea3ee30eb5616dc729
Christophe Ohl <chris.ohl67@gmail.com>
2022-02-09 07:15:34 +0100

    add thread-safe resolvers

--- TreeBranch.Microsoft.Extensions.DependencyInjection/TreeBranchProvider/Services/ServiceValueResolver/ValueResolver/ResolverProxy.cs
+++ TreeBranch.Microsoft.Extensions.DependencyInjection/TreeBranchProvider/Services/ServiceValueResolver/ValueResolver/ResolverProxy.cs
@@ -1,40 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 
 namespace TreeBranch.Microsoft.Extensions.DependencyInjection
 {
     internal class ResolverProxy
     {
-        private int _index = -1;
-        private readonly Type _IEnumerableT;
-        private readonly Type _ServiceType;
+        private int _indexMax = -1;
+        private readonly ThreadLocal<Resolver> _tlResolver;
         public ResolverProxy(Type serviceType)
         {
-            _ServiceType = serviceType;
-            _IEnumerableT = typeof(IEnumerable<>).MakeGenericType(serviceType);
+            _tlResolver = new ThreadLocal<Resolver>(() => new Resolver(_indexMax, typeof(IEnumerable<>).MakeGenericType(serviceType), serviceType));
         }
         internal int IncrementIndex()
         {
-            _index++;
-            return _index;
+            _indexMax++;
+            return _indexMax;
         }
-        public int Count { get { return _index + 1; } }
+        public int Count { get { return _indexMax + 1; } }
 
-        private IReadOnlyList<object>? _values;
         internal object GetValue(IServiceProvider provider, int requestIndex)
         {
-            if (_index != requestIndex && requestIndex == 0)
-                _values = ((IEnumerable<object>)provider.GetRequiredService(_IEnumerableT)).ToArray();
-            if (_values != null)
-            {
-                var result = _values[requestIndex];
-                if ((_values.Count - 1) == requestIndex)
-                    _values = null;
-                return result;
-            }
-            return provider.GetRequiredService(_ServiceType);
+            return _tlResolver.Value.GetValue(provider, requestIndex);
         }
     }
+
 }
