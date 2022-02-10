d5cceb5be6b681209fb1bc93b3e2310871d287ab
Christophe Ohl <chris.ohl67@gmail.com>
2022-02-10 15:32:04 +0100

    add Internal_IServiceSource

---
+++ src/TreeNodes.Test/Internal.Test/Internal_IServiceSource.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreeNodes.Microsoft.Extensions.DependencyInjection;
+
+namespace TreeNodes.Test
+{
+    [TestClass]
+    public class Internal_IServiceSource
+    {
+        private IServiceCollection testServices = new ServiceCollection();
+        public Internal_IServiceSource()
+        {
+            testServices.AddSingleton<IServiceSource, ServiceSource>();
+        }
+        [TestMethod(nameof(IServiceSource))] 
+        public void PreservationSource()
+        {
+            IServiceCollection source = new ServiceCollection();
+            testServices.AddSingleton(source);
+            var testProvider = testServices.BuildServiceProvider();
+            var sourceService = testProvider.GetService<IServiceSource>();
+            Assert.IsNotNull(sourceService);
+            Assert.IsNotNull(sourceService.Source);
+            Assert.AreEqual(source, sourceService.Source);
+            Assert.AreNotEqual(testServices, sourceService.Source);
+        }
+    }
+}
